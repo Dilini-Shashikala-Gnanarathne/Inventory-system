20b25485f5fe3eabba51d994a311c5951196d1ae
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-21 22:45:01 +0530

    created getUserFunction

---
+++ backend/controller/getUser.js
@@ -0,0 +1,34 @@
+const User = require('../models/UserSchema');
+const Admin = require('../models/AdminSchema');
+
+const getUser = async (req, res) => {
+  const { email } = req.body; 
+
+  try {
+    console.log('Request Body:', req.body);
+
+    let record = await User.findOne({ email })|| Admin.findOne({ email});
+    if (record) {
+      return res.status(200).json({
+        success: true,
+        message: 'Successfully retrieved record',
+        data: record,
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: 'Record not found',
+      });
+    }
+
+  } catch (err) {
+    console.error('Error:', err);
+    return res.status(500).json({ success: false, message: 'Internal server error', err: err.message });
+  }
+};
+
+const getUserFunction = (req, res) => getUser(req, res);
+
+module.exports = {
+    getUserFunction,
+};
--- backend/router.js
+++ backend/router.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { authenticateToken, verifyUser } = require('./auth/verifyToken');
 const routers = require('./controller/auth');
 const router = express.Router();
-
+const getUser= require('./controller/getUser');
 
 router.post('/createUser', routers.register);
 router.post('/login', routers.login);
-
+router.get('/getUser', getUser.getUserFunction);
 
 
 router.get('/user',authenticateToken, verifyUser);
