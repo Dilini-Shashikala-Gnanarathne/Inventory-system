f1f441aa87e4a2706e1bc48435b6f7fca2e77e9c
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-08-01 10:52:46 +0530

    Crud functions created

--- demo2/src/main/java/com/example/demo/controller/UserController.java
+++ demo2/src/main/java/com/example/demo/controller/UserController.java
@@ -26,4 +26,13 @@ public class UserController {
         return userService.saveUser(userDTO);
     }
 
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteuser")
+    public UserDTO deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
 }
